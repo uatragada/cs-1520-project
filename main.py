168af927d63a0d60b4f664f0234979d37f81e936
Tyree <tspencer1234@outlook.com>
2022-10-10 18:58:41 +0000

    Initial F.E. push -T.S.

---
+++ main.py
@@ -0,0 +1,13 @@
+import flask
+
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def root():
+    return flask.redirect("/cs-1520-project/LoginPage.html", code=302)
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
