6c07fe694c81b8eeee8f04f2d71b616da4a36358
Tyree <tspencer1234@outlook.com>
2022-11-09 23:45:55 +0000

    working page

--- main.py
+++ main.py
@@ -10,30 +10,30 @@ um = user.UserManager()
 
 @app.route('/')
 def root():
-    return flask.redirect("/cs/LoginPage.html", code=302)
+    return flask.render_template("LoginPage.html", code=302)
 
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
 
 
-@app.route('/register', methods=['POST', 'GET'])
-def register_user():
-    fname = flask.request.form['fname']
-    lname = flask.request.form['lname']
-    email = flask.request.form['email']
-    password = flask.request.form['password']
-    if fname and lname and email and password:
-        um.register_user(fname, lname, email, password)
+# @app.route('/register', methods=['POST', 'GET'])
+# def register_user():
+#     fname = flask.request.form['fname']
+#     lname = flask.request.form['lname']
+#     email = flask.request.form['email']
+#     password = flask.request.form['password']
+#     if fname and lname and email and password:
+#         um.register_user(fname, lname, email, password)
 
 
-@app.route('/login', methods=['POST', 'GET'])
-def login_user():
-    email = flask.request.form['email']
-    password = flask.request.form['password']
-    if email and password:
-        um.login_user(email, password)
+# @app.route('/login', methods=['POST', 'GET'])
+# def login_user():
+#     email = flask.request.form['email']
+#     password = flask.request.form['password']
+#     if email and password:
+#         um.login_user(email, password)
 
-@app.route('/shirtSubmission', methods=['POST', 'GET'])
-def shirt_submission():
-    return flask.redirect("/cs/LoginPage.html", code=302)
+# @app.route('/shirtSubmission', methods=['POST', 'GET'])
+# def shirt_submission():
+#     return flask.redirect("/cs/LoginPage.html", code=302)
