7a88d729240f9472da882193a75cebe2a8946c0e
Lauren <lgh19@pitt.edu>
2022-11-11 00:01:43 +0000

    Add base navigation template, implement show_page function

--- main.py
+++ main.py
@@ -14,14 +14,18 @@ sm = shirts.ShirtManager()
 
 @app.route('/')
 def root():
-    return flask.render_template("LoginPage.html", code=302)
+    curr_user = get_user()
+    if curr_user:
+        return flask.redirect('/products')
+    else:
+        return flask.redirect('/login')
 
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
 
 
-@app.route('/Register.html', methods=['POST', 'GET'])
+@app.route('/register', methods=['POST', 'GET'])
 def register_user():
     if flask.request.method =='POST':
         fname = flask.request.form['fname']
@@ -31,38 +35,38 @@ def register_user():
         acc_type = flask.request.form['account-type']
         password = get_password_hash(flask.request.form['password'])
         if not user.is_unique("email", email):
-            return flask.render_template("Register.html", error_message='Account with this email already exists')
+            return show_page("Register.html", err='Account with this email already exists')
         if not user.is_unique("username", uname):
-            return flask.render_template("Register.html", error_message='Username not available')
+            return show_page("Register.html", err='Username not available')
         elif fname and lname and uname and email and password and acc_type:
             flask.session['user'] = um.register_user(fname, lname, uname, email, password, acc_type)
-            return flask.redirect('/ProductPage.html')
-    return flask.render_template("Register.html", code=302)
+            return flask.redirect('/products')
+    return show_page("/Register.html")
     
 
-@app.route('/LoginPage.html', methods=['POST', 'GET'])
+@app.route('/login', methods=['POST', 'GET'])
 def login_user():
     if flask.request.method == 'POST':
         email = flask.request.form['email']
-        password = hash(flask.request.form['password'])
+        password = get_password_hash(flask.request.form['password'])
         if email and password:
             flask.session['user'] = um.login_user(email, password)
             curr_user = get_user()
             if curr_user is not None:
-                return flask.redirect('/ProductPage.html')
+                return flask.redirect('/products')
             else:
-                return flask.render_template('/LoginPage.html', error_message='Incorrect username or password')
-    return flask.render_template("LoginPage.html", code=302)
+                return show_page('/LoginPage.html', err='Incorrect email or password')
+    return show_page("/LoginPage.html")
 
-@app.route('/About.html')
+@app.route('/about')
 def About():
-    return flask.render_template("About.html", code=302)
+    return show_page("/About.html")
 
-@app.route('/ProductPage.html', methods = ['GET'])
+@app.route('/products', methods = ['GET'])
 def ProductPage():
-    return flask.render_template("ProductPage.html", code=302)
+    return show_page("/ProductPage.html")
 
-@app.route('/ShirtSubmission.html', methods=['POST', 'GET'])
+@app.route('/shirt-submission', methods=['POST', 'GET'])
 def shirt_submission():
     if flask.request.method == 'POST':
         designName = flask.request.form["shirtName"]
@@ -71,12 +75,14 @@ def shirt_submission():
         if curr_user:
             sm.new_design(curr_user['first_name'],designName, shirtImage)
             print("Saved New Design")
-            return flask.redirect("/ShirtConfirmation.html", code=302)
-    return flask.render_template("ShirtSubmission.html", code=302)
+            return flask.redirect('/shirt-confirmation')
+    return show_page("ShirtSubmission.html")
 
-@app.route('/ShirtConfirmation.html', methods=['POST', 'GET'])
+
+@app.route('/shirt-confirmation', methods=['POST', 'GET'])
 def shirt_confirmation():
-    return flask.render_template("ShirtConfirmation.html", code=302)
+    return show_page("ShirtConfirmation.html")
+
 
 def get_user():
     return flask.session.get('user', None)
@@ -85,6 +91,10 @@ def get_password_hash(pw):
     encoded = pw.encode('utf-8')
     return hashlib.sha256(encoded).hexdigest()
 
+@app.route('/logout')
 def logout():
     flask.session['user'] = None
-    return flask.redirect('LoginPage.html')
+    return flask.redirect('/login')
+
+def show_page(page, err=None):
+    return flask.render_template(page, current_user=get_user(), error_message=err)
